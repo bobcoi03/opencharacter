f0d5ef0228502eafadee77a16d3671cfcbeb0b3e
bobcoi03 <elongatedmusk6969@gmail.com>
2024-10-02 00:22:54 +0100

    got rid of room

--- src/components/new-sidebar.tsx
+++ src/components/new-sidebar.tsx
@@ -280,15 +280,6 @@ const SidebarContent: React.FC<SidebarContentProps> = ({
               variant="ghost"
               className="w-full justify-start rounded-none text-left px-4 py-2 text-sm text-gray-300 hover:bg-neutral-700"
               onClick={() => {
-                router.push("/room/create");
-              }}
-            >
-              Room
-            </Button>
-            <Button
-              variant="ghost"
-              className="w-full justify-start rounded-none text-left px-4 py-2 text-sm text-gray-300 hover:bg-neutral-700"
-              onClick={() => {
                 router.push("/profile/persona/create");
               }}
             >
