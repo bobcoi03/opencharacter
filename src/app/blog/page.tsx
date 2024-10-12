1b539f0c5502f4a967e601c00689dada5ad4b2d3
bobcoi03 <elongatedmusk6969@gmail.com>
2024-10-12 01:12:42 +0100

    blog changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,8 +10,8 @@ interface BlogPost {
 }
 
 const BlogCard: React.FC<BlogPost> = ({ image, category, title, excerpt, link }) => (
-  <Link href={link} className="block overflow-hidden transition-opacity max-w-md rounded-lg shadow-lg">
-    <img src={image} alt={title} className="w-full h-64 object-cover" />
+  <Link href={link} className="block overflow-hidden transition-opacity max-w-md rounded-xl">
+    <img src={image} alt={title} className="w-full h-64 rounded-xl" />
     <div className="p-6">
       <p className="text-indigo-400 text-sm mb-2">{category}</p>
       <h3 className="text-xl font-bold text-white mb-2">{title}</h3>
@@ -30,13 +30,13 @@ const BlogPage: React.FC = () => {
   };
 
   return (
-    <div className="bg-neutral-900 min-h-screen text-white">
-      <div className="relative h-[50vh] flex items-center justify-center">
+    <div className="bg-neutral-900 min-h-screen text-white mb-24">
+      <div className="mx-auto relative h-[50vh] flex items-center justify-center">
         <div className="absolute inset-0 z-0 md:ml-16 rounded-xl overflow-hidden">
           <img src="/blog-background.webp" alt="Background" className="w-full h-full object-cover rounded-xl" />
         </div>
         <div className="relative z-10 text-center px-4">
-          <h1 className="text-5xl font-bold mb-4 text-black">OpenCharacter.org</h1>
+          <h1 className="text-wrap text-2xl md:text-5xl font-bold mb-4 text-black">OpenCharacter.org</h1>
           <p className="text-sm text-black max-w-2xl mx-auto">
             The open source, uncensored, free alternative to CharacterAI
           </p>
