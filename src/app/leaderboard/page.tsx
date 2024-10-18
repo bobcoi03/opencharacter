cb32c038ecd3287a9c0246dad76c43875a16412f
bobcoi03 <elongatedmusk6969@gmail.com>
2024-10-18 01:28:21 +0100

    add margin to bottom

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -40,7 +40,7 @@ export default async function LeaderBoard() {
     const leaderboardData = await getLeaderboardData();
 
     return (
-        <div className="container mx-auto px-2 sm:px-4 py-4 sm:py-8">
+        <div className="container mx-auto px-2 sm:px-4 py-4 sm:py-8 mb-24">
             <h1 className="text-2xl sm:text-3xl font-bold mb-4 sm:mb-6 text-center text-white">Creator Leaderboard Top 100</h1>
             <div className="grid gap-2 sm:gap-4">
                 {leaderboardData.map((user, index) => (
