84db54e57e751ca73c91415e79d8e26843ffb2eb
bobcoi03 <elongatedmusk6969@gmail.com>
2024-11-02 14:30:47 +0000

    added new paid u

--- src/app/actions/chat.ts
+++ src/app/actions/chat.ts
@@ -28,6 +28,7 @@ const PAID_USER_IDS = [
   "221e44d6-9dee-4d73-a5e3-5cae0f8ead1c", // P 25/10/24
   "2c435f13-5ff7-434f-93c1-b3c0c90efe9b", // P friend 25/10/24,
   "3fd781ff-8ad6-4a4c-806f-4a58aaa17526", // w 26/10/24
+  "918eb513-1b0c-4461-b07f-350d264540e4" // s 02/11/24
 ];
 
 type ErrorResponse = {
