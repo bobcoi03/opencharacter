36d6d838a2e3a6fd9843038ffe9ee0a2d21185ca
bobcoi03 <elongatedmusk6969@gmail.com>
2024-11-25 02:02:44 +0000

    make typesafe

--- src/app/actions/chat.ts
+++ src/app/actions/chat.ts
@@ -202,8 +202,8 @@ export async function continueConversation(
       apiKey: process.env.DAW_API_KEY,
       headers: {
         Authorization: `Bearer ${process.env.DAW_API_KEY}`,
-        UserID: session.user.id,
-        SessionID: chatSession.id
+        UserID: session.user.id!,
+        SessionID: chat_session_id ?? ""
       }
     })
   } else {
