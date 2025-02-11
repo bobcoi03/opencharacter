c59cc0360319f00bdd251428a7e532312a0002f2
bobcoi03 <elongatedmusk6969@gmail.com>
2025-02-11 23:03:51 +0000

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,25 +202,6 @@ export default async function Page({
         />
       </Suspense>
 
-      {/* Goonmaps Banner Ad */}
-      <div className="w-full flex flex-col items-center my-8">
-        <p className="text-sm text-gray-400 mb-2">Special Limited Time Ad For Goonmaps!</p>
-        <a 
-          href="https://www.goonmaps.com/?ref=opencharacter.org" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="hover:opacity-90 transition-opacity"
-        >
-          <img
-            src="/goonmaps.png"
-            alt="Goonmaps - Visit Now"
-            className="rounded-lg shadow-lg object-cover"
-            width={200}
-            height={10}
-          />
-        </a>
-      </div>
-
       <Footer />
     </div>
   );
