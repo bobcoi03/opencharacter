511c0b81ce2b7715b2db9c5b4cdd1cceae8084d4
bobcoi03 <elongatedmusk6969@gmail.com>
2025-02-23 00:04:20 +0000

    upgrade to pro cta

--- src/app/chat/[character_id]/messages-and-input.tsx
+++ src/app/chat/[character_id]/messages-and-input.tsx
@@ -836,7 +836,7 @@ export default function MessageAndInput({
         </Link>
       }
       {!share && 
-        <div className="fixed bottom-0 left-0 right-0 py-4 pointer-events-none w-full max-w-full">
+        <div className="fixed bottom-0 left-0 right-0 py-4 w-full max-w-full">
         <div className="max-w-2xl mx-auto w-full">
           {error && (
             <div className="mb-2 p-2 bg-red-900 border border-red-800 rounded-lg text-red-200 text-sm pointer-events-auto flex justify-between items-center">
@@ -860,6 +860,15 @@ export default function MessageAndInput({
               />
             </div>
           )}
+
+          <div className="flex flex-col items-center justify-center mb-1">
+
+            <Link href="/plans" className="text-center justify-center text-[9px] text-blue-500 underline ">
+              Upgrade to Pro
+            </Link>
+
+          </div>
+
           <form
             ref={formRef}
             onSubmit={handleFormSubmit}
@@ -874,6 +883,7 @@ export default function MessageAndInput({
                 />
               </div>
               <textarea
+                autoFocus
                 ref={textareaRef}
                 name="message"
                 onChange={handleInputChange}
